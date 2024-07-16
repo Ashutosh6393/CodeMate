3218fa2b73e7760ae3e445edc51ca66986024517
Ashutosh Verma <vashutosh625@gmail.com>
2024-07-16 20:18:17 +0530

    changed file name

---
+++ Backend/index.js
@@ -0,0 +1,71 @@
+import http from "http";
+import path from "path";
+import express from "express";
+import { Server } from "socket.io";
+import { EVENTS } from "./events.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+const getRoomMembers = async (roomId) => {
+  let members = [];
+  io.in(roomId)
+    .fetchSockets()
+    .then((data) => {
+      members = data.map((item) => {
+        return { socketId: item.id, username: item.data.username };
+      });
+
+      io.to(roomId).emit(EVENTS.ROOM_MEMBERS, members);
+    });
+};
+
+io.on("connection", (socket) => {
+  socket.on(EVENTS.JOIN, async ({ roomId, username }) => {
+    socket.data.username = username;
+    socket.data.roomId = roomId;
+    socket.join(roomId);
+    getRoomMembers(roomId);
+    io.to(roomId).emit(EVENTS.NEW_MEMBER, { username, socketId: socket.id });
+  });
+
+  socket.on(EVENTS.JOIN_CODESPACE, (data) => {
+    socket.join(data.userToWatch);
+    socket.to(data.userToWatch).emit(EVENTS.GET_CODE);
+  });
+
+  socket.on(EVENTS.SEND_CODE_TO_SUBSCRIBERS, (data) => {
+    // console.log(data.toString());
+    io.to(socket.id).emit(EVENTS.SUBSCRIBED_CODE, {
+      sender: socket.id,
+      data: data,
+    });
+  });
+
+  socket.on(EVENTS.LEAVE_CODESPACE, (socketId) => {
+    socket.leave(socketId.currentlyWatching);
+  });
+
+  socket.on(EVENTS.RUN, async ({ code, language }) => {
+    const output = await runCode({ code, language });
+    console.log("output:ev ",output);
+  });
+
+  socket.on("disconnect", () => {
+    io.to(socket.data.roomId).emit(
+      EVENTS.LEAVE,
+      { username: socket.data.username, socketId: socket.id },
+      () => {
+        getRoomMembers(socket.data.roomId);
+        socket.leave(socket.data.roomId);
+      }
+    );
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
