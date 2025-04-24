48120e71c6bdfcb7f6f290678a08e5cf05761425
Ashutosh Verma <vashutosh625@gmail.com>
2025-04-24 16:25:54 +0530

    create menubar

--- apps/client/src/components/layout/Navbar.tsx
+++ apps/client/src/components/layout/Navbar.tsx
@@ -1,9 +1,22 @@
 import { IoMdSettings } from "react-icons/io";
 import { BiSolidUser } from "react-icons/bi";
+import { Switch } from "@/components/ui/switch";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext.tsx";
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarLabel,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Button } from "../ui/button.tsx";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const {user} = useContext(AuthContext);
   return (
     <nav className="w-full bg-zinc-900 py-5 text-white px-10 flex justify-between items-center">
       <h1 className="text-3xl font-pp">
@@ -11,8 +24,52 @@ const Navbar = (props: Props) => {
         <span className="text-purple-800">M</span>ate
       </h1>
       <div className="flex justify-center items-center gap-10">
-        <IoMdSettings className="text-2xl text-white hover:scale-110 hover:rotate-90 transition-all" />
-        <BiSolidUser className="text-2xl text-white hover:scale-110 " />
+        <Menubar className="bg-transparent border-0 text-white">
+          <MenubarMenu>
+            <MenubarTrigger>
+              <IoMdSettings className="text-2xl text-white hover:scale-110 hover:rotate-90 transition-all" />
+            </MenubarTrigger>
+            <MenubarContent className="p-2">
+              <div className="flex justify-between items-center gap-2.5">
+                <MenubarLabel className="text-zinc-700 text-sm  font-normal">
+                  Allow other to edit
+                </MenubarLabel>
+                <Switch />
+              </div>
+              <div className="flex justify-between items-center gap-2.5">
+                <MenubarLabel className="text-zinc-700 text-sm  font-normal">
+                  Live Share
+                </MenubarLabel>
+                <Switch />
+              </div>
+              <div className="flex justify-between items-center gap-2.5">
+                <MenubarLabel className="text-zinc-700 text-sm  font-normal">
+                  Invite Link
+                </MenubarLabel>
+                <Button
+                  className="p-0 text-zinc-600 hover:text-zinc-950"
+                  variant={"ghost"}
+                >
+                  Copy
+                </Button>
+              </div>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <BiSolidUser className="text-2xl text-white hover:scale-110 " />
+            </MenubarTrigger>
+            <MenubarContent className="flex flex-col ">
+              <MenubarLabel className="text-zinc-700 text-sm  font-normal">
+                {user && (user.name.charAt(0).toUpperCase()+ user.name.slice(1))}
+              </MenubarLabel>
+              <MenubarLabel className="text-zinc-700 text-sm  font-normal">
+                {user?.email}
+              </MenubarLabel>
+              <Button className="mt-3">Logout</Button>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
       </div>
     </nav>
   );
